89a4445f9de8d65894ff0e670c4f215c33666f99
syawalqi <galihmaulanasyawalqi@gmail.com>
2026-05-15 22:19:31 +0700

    fix: add security headers middleware (X-Frame-Options, X-Content-Type-Options, X-XSS-Protection, Referrer-Policy)

--- laporin/bootstrap/app.php
+++ laporin/bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
